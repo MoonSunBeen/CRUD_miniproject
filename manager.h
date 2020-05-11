0f04bc6a57ed7e18146a47c874d12554ed4b6eb9
MoonSunBeen <54530356+MoonSunBeen@users.noreply.github.com>
2020-05-11 23:50:57 +0900

    Create manager.h

---
+++ manager.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+	char market[60];
+	char phone[60];
+	char menu[60];
+	int price;
+	int star;
+} H_delivery;
+
+//상품 조회
+void listdatadelivery(H_delivery *H, int count);
+//상품추가
+int adddelivery(H_delivery *H);
+//상품수정
+int updatedelivery(H_delivery *H);
+//상품삭제
+int deletedelivery(H_delivery *H);
+//상품 번호 조회
+int selectDataNo(H_delivery *H, int count);
+//메뉴 조회
+int selectdelivery();
+//파일 저장
+void savedatadelivery(H_delivery *H, int count);
+//파일 로드
+int loaddatadelivery(H_delivery *H);
